3482d491f64a7affb6c87ac64814b73e3e74ffd5
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-08-17 09:59:46 +0900

    fix bugs

--- contracts/Reentrancy.sol
+++ contracts/Reentrancy.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Security is ReentrancyGuard, Ownable {
+contract Reentrancy is ReentrancyGuard, Ownable {
 
     event Received(address, uint);
 
